c4807aa40ef90657f2c9c5de85c83b6ee67a01b0
qwezxcrvrv@gmail.com <qwezxcrvrv@gmail.com>
2017-08-16 16:14:28 +1200

    Set win result - change squares color

--- colorGame.js
+++ colorGame.js
@@ -10,6 +10,7 @@ var colors = [
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
+var message = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,10 +24,20 @@ for(var i = 0; i < squares.length; i++){
 		var clickedColor = this.style.backgroundColor;
 		//compare color to pickedColor
 		if(clickedColor === pickedColor){
-			alert("Correct");
+			messageDisplay.textContent = "Correct!"
+			changeColors(clickedColor);
 		}else{
-			alert("Wrong");
-
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try Again"
 		}
 	})
+}
+
+function changeColors(color){
+	//loop through all squares
+	for(var i = 0; i < squares.length; i++){
+		//change each color to match given color
+		squares[i].style.backgroundColor = color;
+	}
+	
 }
