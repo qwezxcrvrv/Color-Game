154781edbb0c10306e8f8514dde67522322a6781
qwezxcrvrv@gmail.com <qwezxcrvrv@gmail.com>
2017-08-17 17:04:41 +1200

    Add easy mode and enhance design

--- colorGame.js
+++ colorGame.js
@@ -1,19 +1,55 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var message = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
 
 colorDisplay.textContent = pickedColor;
+
+
+//Easy mode
+easyBtn.addEventListener("click",function(){
+	easyBtn.classList.add("#selected");
+	hardBtn.classList.remove("#selected");
+	numSquares = 3;
+
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for (var i = 0; i < squares.length; i++) {
+		if (colors[i]) {
+			squares[i].style.backgroundColor = colors[i];
+		} else{
+			squares[i].style.display = "none";
+		}
+	}
+})
+
+hardBtn.addEventListener("click",function(){
+	easyBtn.classList.remove("#selected");
+	hardBtn.classList.add("#selected");
+	numSquares = 6;
+
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = colors[i];
+		squares[i].style.display = "block";
+	}
+})
 
 resetButton.addEventListener("click",function(){
 	messageDisplay.textContent = "";
 	resetButton.textContent = "Play Again?";
 	//generate all new colors
-	colors = generateRandomColors(6);
+	colors = generateRandomColors(numSquares);
 	//pick a new random colors
 	pickedColor = pickColor();
 	//change colorDisplay to match picked Color
@@ -22,7 +58,7 @@ resetButton.addEventListener("click",function(){
 	for (var i = 0; i < squares.length; i++) {
 		squares[i].style.backgroundColor = colors[i];
 	}
-	h1.style.backgroundColor = "#232323";
+	h1.style.backgroundColor = "steelblue";
 })
 
 for(var i = 0; i < squares.length; i++){
